319421b957573a8fc6da2e7bda4dfcc096ad3cda
Lex O'brien <robert.arbon@gmail.com>
2024-05-27 09:19:36 +0000

    removing log validation

--- Development/fine_tune_riffusion_v3.py
+++ Development/fine_tune_riffusion_v3.py
@@ -503,25 +503,25 @@ def training_loop(args):
             if global_step >= args.max_train_steps:
                 break
     
-        if accelerator.is_main_process:
-            if args.validation_prompts is not None and epoch % args.validation_epochs == 0:
-                if args.use_ema:
-                    # Store the UNet parameters temporarily and load the EMA parameters to perform inference.
-                    ema_unet.store(unet.parameters())
-                    ema_unet.copy_to(unet.parameters())
-                log_validation(
-                    vae,
-                    text_encoder,
-                    tokenizer,
-                    unet,
-                    args,
-                    accelerator,
-                    weight_dtype,
-                    global_step,
-                )
-                if args.use_ema:
-                    # Switch back to the original UNet parameters.
-                    ema_unet.restore(unet.parameters())
+        # if accelerator.is_main_process:
+        #     if args.validation_prompts is not None and epoch % args.validation_epochs == 0:
+        #         if args.use_ema:
+        #             # Store the UNet parameters temporarily and load the EMA parameters to perform inference.
+        #             ema_unet.store(unet.parameters())
+        #             ema_unet.copy_to(unet.parameters())
+        #         log_validation(
+        #             vae,
+        #             text_encoder,
+        #             tokenizer,
+        #             unet,
+        #             args,
+        #             accelerator,
+        #             weight_dtype,
+        #             global_step,
+        #         )
+        #         if args.use_ema:
+        #             # Switch back to the original UNet parameters.
+        #             ema_unet.restore(unet.parameters())
     
     # Create the pipeline using the trained modules and save it.
     accelerator.wait_for_everyone()
